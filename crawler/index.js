fd166a07d016e6c0e4440c84378e975c2fd5a294
workmichaelma <work.michael.ma@gmail.com>
2019-03-02 03:45:47 +0800

    Add multiple discount offer

--- crawler/index.js
+++ crawler/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const Crawler = require("crawler");
+const map = require('lodash.map')
+const words = require('lodash/words')
 const app = express();
 
 app.get('/:id', (req, res) => {
@@ -15,10 +17,26 @@ app.get('/:id', (req, res) => {
           // $ is Cheerio by default
           //a lean implementation of core jQuery designed specifically for the server
       }
+
+      let special = $('#item-photo-container .offer-table')
+      let specialOffer
+      if (special && special.children().length) {
+        specialOffer = $(special.children().map((i, child) => {
+          child = $(child)
+          price = child.find('span.price')
+          return ( {
+            amount: child.attr('data-value'),
+            price: $(price).contents().map((n, p) => {
+              return p.type === 'text' ? words($(p).text(), /[_A-Za-z0-9.@-]+$/g).join('') : ''
+            }).get().join('')
+          })
+        })).get()
+      }
       
       res.json({
         'title': $("title").text(),
-        'price': $("#item-photo-container .price-container span[itemprop=price]").attr('content')
+        'price': $("#item-photo-container .price-container span[itemprop=price]").attr('content'),
+        'special-offer': special && specialOffer ? specialOffer : undefined,
       })
       done();
     }
