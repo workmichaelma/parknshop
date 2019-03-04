e8b33351cceb802fe54449661d0c6ae8bf30aeb7
workmichaelma <work.michael.ma@gmail.com>
2019-03-04 22:51:05 +0800

    Update API to serve Product

---
+++ crawler/Product.js
@@ -0,0 +1,59 @@
+const words = require('lodash/words')
+const trimEnd = require('lodash/trimEnd')
+
+module.exports = {
+  title($) {
+    const ele = $('#item-photo-container div.itemName')    
+    return ele ? ele.text() : ''
+  },
+  prices($) {
+    let ele = $('#item-photo-container .offer-table')
+    let prices = []
+    if (ele && ele.children().length) {
+      prices = $(ele.children().map((i, child) => {
+        child = $(child)
+        price = child.find('span.price')
+        return ( {
+          amount: child.attr('data-value'),
+          price: $(price).contents().map((n, p) => {
+            return p.type === 'text' ? words($(p).text(), /[_A-Za-z0-9.@-]+$/g).join('') : ''
+          }).get().join('')
+        })
+      })).get()
+    }
+    return prices
+  },
+  categories($) {
+    const ele = $('.categoryAndBrandContainer ul')
+    let categories = []
+    if (ele && ele.children().length) {
+      categories = $(ele.children().map((i, child) => {
+        child = $(child)
+        const href = child.find('a').attr('href').split('/')
+        return {
+          title: trimEnd(child.text()),
+          code: href.pop(),
+          type: href.indexOf(`brandlist`) > 0 ? 'brand': 'category'
+        }
+      })).get()
+    }
+    return categories
+  },
+  image($) {
+    return $(`meta[property="og:image"]`).attr('content') || ''
+  },
+  init($, detail) {
+    if (detail) {
+      return {
+        title: this.title($),
+        image: this.image($),
+        categories: this.categories($),
+        prices: this.prices($),
+      }
+    } else {
+      return {
+        prices: this.prices($),
+      }
+    }
+  }
+}
--- crawler/index.js
+++ crawler/index.js
@@ -1,48 +1,35 @@
 const express = require('express');
 const Crawler = require("crawler");
 const map = require('lodash.map')
-const words = require('lodash/words')
 const app = express();
 
-app.get('/:id', (req, res) => {
+const Product = require('./Product')
+
+app.get('/:id/:detail?', (req, res) => {
   var id = req.params.id
-  var c = new Crawler({
-    maxConnections : 10,
-    // This will be called for each crawled page
-    callback : function (error, doc, done) {
-      if(error){
+  var detail = req.params.detail || false
+  
+  try {
+    var c = new Crawler({
+      maxConnections: 10,
+      // This will be called for each crawled page
+      callback: function (error, doc, done) {
+        if (error) {
           console.log(error);
-      }else{
+        } else {
           var $ = doc.$;
+          res.json(Product.init($, detail))
           // $ is Cheerio by default
           //a lean implementation of core jQuery designed specifically for the server
+        }
+        done();
       }
-
-      let special = $('#item-photo-container .offer-table')
-      let specialOffer
-      if (special && special.children().length) {
-        specialOffer = $(special.children().map((i, child) => {
-          child = $(child)
-          price = child.find('span.price')
-          return ( {
-            amount: child.attr('data-value'),
-            price: $(price).contents().map((n, p) => {
-              return p.type === 'text' ? words($(p).text(), /[_A-Za-z0-9.@-]+$/g).join('') : ''
-            }).get().join('')
-          })
-        })).get()
-      }
-      
-      res.json({
-        'title': $("title").text(),
-        'price': $("#item-photo-container .price-container span[itemprop=price]").attr('content'),
-        'special-offer': special && specialOffer ? specialOffer : undefined,
-      })
-      done();
-    }
-  });
-  
-  c.queue(`https://www.parknshop.com/zh-hk/code/p/${id}`);
+    });
+    
+    c.queue(`https://www.parknshop.com/zh-hk/code/p/${id}`);
+  } catch (err) {
+    res.json({})
+  }
 
 })
 
