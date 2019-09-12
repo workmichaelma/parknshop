83a26a1f795e64c77a5cae412d56f2ab4e3d75c0
workmichaelma <work.michael.ma@gmail.com>
2019-09-13 00:24:45 +0800

    update host port

--- nuxt/api/apollo-client.js
+++ nuxt/api/apollo-client.js
@@ -1,6 +1,15 @@
 import ApolloClient from 'apollo-boost';
 
-const uri = (process.env.VUE_ENV === 'server') ? 'http://api:3000/graphql' : 'http://127.0.0.1:1003/graphql'
+let uri
+if (process.env.VUE_ENV === 'server') {
+  uri = 'http://api:3000/graphql'
+} else {
+  if (process.env.NODE_ENV === 'development') {
+    uri = 'http://127.0.0.1:1003/graphql'
+  } else {
+    uri = 'http://159.65.132.237:1003/graphql'
+  }
+}
 
 const client = new ApolloClient({
   uri
